7d8d3263d6d099eab3683ab9f6b9fd88d7ceaf8c
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-05-08 12:02:52 +0200

    improve example

--- docs/neovim-config/add-to-lsp.lua
+++ docs/neovim-config/add-to-lsp.lua
@@ -2,11 +2,16 @@ local configs = require("lspconfig.configs")
 
 configs.nobl9_language_server = {
   default_config = {
-    cmd = { "nobl9-language-server" },
+    cmd = {
+      "nobl9-language-server",
+      "-logFilePath=~/.local/state/nobl9-language-server/n9.log",
+      "-logLevel=ERROR",
+    },
     filetypes = { "yaml" },
     root_dir = function(fname)
       return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
     end,
+    single_file_support = true,
     settings = {},
   },
 }
--- docs/neovim-config/init.lua
+++ docs/neovim-config/init.lua
@@ -80,6 +80,7 @@ configs.nobl9_language_server = {
     root_dir = function(fname)
       return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
     end,
+    single_file_support = true,
     settings = {},
   },
 }
